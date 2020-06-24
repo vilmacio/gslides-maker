d9dbcdc02b3dbe6cf455d91e14a1f37f5aa85192
vilmacio22 <vilmacio22@users.noreply.github.com>
2020-06-24 15:53:01 -0300

    bug fixes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import data from './main/data'
 import userInput from './main/userInput'
 import text from './main/text'
-import images from './main/images'
-import slides from './main/slides'
+// import images from './main/images'
+// import slides from './main/slides'
 
 async function start ():Promise<void> {
   await userInput(data)
   await text(data)
-  await images(data)
+  // await images(data)
   console.log(JSON.stringify(data))
-  await slides(data)
+  // await slides(data)
 }
 start()
