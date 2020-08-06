dd310b2af3ca510f9f4e8c1df35ba4fd83a5fc4e
vilmacio22 <vilmacio22@users.noreply.github.com>
2020-08-05 22:51:28 -0300

    question mark

--- src/main/userInput.ts
+++ src/main/userInput.ts
@@ -2,6 +2,7 @@ import readline from 'readline-sync'
 import getArticlesArray from '../services/algorithmia'
 import { Data } from './data'
 import { bold } from '../log'
+import chalk from 'chalk'
 
 export default async function userInput (data:Data):Promise<Data> {
   console.log(bold('gslides-maker v0.5 [beta]'))
@@ -10,19 +11,22 @@ export default async function userInput (data:Data):Promise<Data> {
   data.input.lang = await getLang()
 
   function getSearch ():string {
-    const search = readline.question('What do you want to research about? ')
+    const search = readline.question(`${chalk.bold.green('?')} What do you want to research about? `)
     return search
   }
 
   async function getArticle (fetchArticles: Promise<Array<string>>):Promise<string> {
     const articles = await fetchArticles
-    const selectedIndex = readline.keyInSelect(articles, 'Choose an Wikipedia article: ')
+    const selectedIndex = readline.keyInSelect(articles, `${chalk.bold.green('>')} Choose an Wikipedia article: `)
+    if (selectedIndex === -1) {
+      process.exit(0)
+    }
     const selectedArticle = articles[selectedIndex]
     return selectedArticle
   }
 
   async function getLang ():Promise<string> {
-    let answer = readline.question('What is the language of the article [en]? ')
+    let answer = readline.question(`${chalk.bold.green('?')} What is the language of the article [en]? `)
     if (!answer) {
       answer = 'en'
     }
